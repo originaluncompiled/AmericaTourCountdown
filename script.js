2dba1a4f75645865f0e06c66613d77c9a8b888ff
originaluncompiled <88604752+originaluncompiled@users.noreply.github.com>
2024-05-22 10:05:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Big number displays
 const monthText = document.getElementById("months");
 const weekText = document.getElementById("weeks");
 const dayText = document.getElementById("days");
@@ -5,17 +6,23 @@ const hourText = document.getElementById("hours");
 const minText = document.getElementById("minutes");
 const secText = document.getElementById("seconds");
 let timeText = [monthText, weekText, dayText, hourText, minText, secText];
+
+// Total time left displays
 const monthTotal = document.getElementById("monthsTotal");
 const weekTotal = document.getElementById("weekTotal");
 const dayTotal = document.getElementById("dayTotal");
 const hourTotal = document.getElementById("hourTotal");
 const minTotal = document.getElementById("minTotal");
 const secTotal = document.getElementById("secTotal");
+let totalTimeText = [monthTotal, weekTotal, dayTotal, hourTotal, minTotal, secTotal];
 
 let timeValues = [];
 
+const formatter = new Intl.NumberFormat('en-UK', { useGrouping: true });
+
 // Set the date we're counting down to
 const countDownDate = new Date("Sept 17, 2024 00:00:00").getTime();
+
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -26,6 +33,7 @@ var x = setInterval(function() {
     // Find the difference between now and the count down date
     var difference = countDownDate - now;
 
+    // How much total time is left
     monthTotal.textContent = `Total: ${Math.floor(difference / (1000 * 60 * 60 * 24 * 30.325))}`;
     weekTotal.textContent = `Total: ${Math.floor(difference / (1000 * 60 * 60 * 24 * 7))}`;
     dayTotal.textContent = `Total: ${Math.floor(difference / (1000 * 60 * 60 * 24))}`;
@@ -52,6 +60,13 @@ var x = setInterval(function() {
         timeText.forEach((textUnit) => {
             textUnit.textContent = "00";
         });
+    }
+    
+    const formatTotalTime = () => {
+        totalTimeText.map(unit => {
+            unit = formatter.format(unit);
+            unit.textContent = `Total ${unit}`;
+        })
     }
 }, 1000);
 
