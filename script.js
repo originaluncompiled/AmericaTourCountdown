87b191e0b58f83fbb5d173141c0a0eacba98a6c3
carstendold1@gmail.com <88604752+OriginalUncompiled@users.noreply.github.com>
2024-05-22 10:32:40 +0200

    yes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Big number displays
 const monthText = document.getElementById("months");
 const weekText = document.getElementById("weeks");
 const dayText = document.getElementById("days");
@@ -5,43 +6,47 @@ const hourText = document.getElementById("hours");
 const minText = document.getElementById("minutes");
 const secText = document.getElementById("seconds");
 let timeText = [monthText, weekText, dayText, hourText, minText, secText];
+
+// Total time left displays
 const monthTotal = document.getElementById("monthsTotal");
 const weekTotal = document.getElementById("weekTotal");
 const dayTotal = document.getElementById("dayTotal");
 const hourTotal = document.getElementById("hourTotal");
 const minTotal = document.getElementById("minTotal");
 const secTotal = document.getElementById("secTotal");
+let totalTimeText = [monthTotal, weekTotal, dayTotal, hourTotal, minTotal, secTotal];
 
-let timeValues = [];
+// Amount of time in certain units
+const monthMillisec = (1000 * 60 * 60 * 24 * 30.325);
+const weekMillisec = (1000 * 60 * 60 * 24 * 7);
+const dayMillisec = (1000 * 60 * 60 * 24);
+const hourMillisec = (1000 * 60 * 60);
+const minMillisec = (1000 * 60);
+const secMillisec = (1000);
 
-// Set the date we're counting down to
+// Format the total remaining time's values
+const timeFormatter = new Intl.NumberFormat('en-UK', { useGrouping: true });
+
+// When we are counting down to
 const countDownDate = new Date("Sept 17, 2024 00:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+const x = setInterval(function() {
     // Get today's date and time
-    var now = new Date().getTime();
+    let now = new Date().getTime();
 
     // Find the difference between now and the count down date
-    var difference = countDownDate - now;
+    let difference = countDownDate - now;
 
-    monthTotal.textContent = `Total: ${Math.floor(difference / (1000 * 60 * 60 * 24 * 30.325))}`;
-    weekTotal.textContent = `Total: ${Math.floor(difference / (1000 * 60 * 60 * 24 * 7))}`;
-    dayTotal.textContent = `Total: ${Math.floor(difference / (1000 * 60 * 60 * 24))}`;
-    hourTotal.textContent = `Total: ${Math.floor(difference / (1000 * 60 * 60))}`;
-    minTotal.textContent = `Total: ${Math.floor(difference / (1000 * 60))}`;
-    secTotal.textContent = `Total: ${Math.floor(difference / (1000))}`;
+    // Time remaining calculations
+    let months = Math.floor(difference / monthMillisec);
+    let weeks = Math.floor((difference % monthMillisec) / weekMillisec);
+    let days = Math.floor((difference % weekMillisec) / dayMillisec);
+    let hours = Math.floor((difference % dayMillisec) / hourMillisec);
+    let minutes = Math.floor((difference % hourMillisec) / minMillisec);
+    let seconds = Math.floor((difference % minMillisec) / secMillisec);
 
-    // Time calculations
-    var months = Math.floor(difference / (1000 * 60 * 60 * 24 * 30.325));
-    var weeks = Math.floor((difference % (1000 * 60 * 60 * 24 * 30.325)) / (1000 * 60 * 60 * 24 * 7));
-    var days = Math.floor((difference % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-    timeValues = [months, weeks, days, hours, minutes, seconds];
+    const timeValues = [months, weeks, days, hours, minutes, seconds];
 
     // Display the result
     if (difference > 0) {
@@ -53,6 +58,21 @@ var x = setInterval(function() {
             textUnit.textContent = "00";
         });
     }
+
+    // Total time remaining calculations:
+    let totalMonths = Math.floor(difference / monthMillisec);
+    let totalWeeks = Math.floor(difference / weekMillisec);
+    let totalDays= Math.floor(difference / dayMillisec);
+    let totalHours = Math.floor(difference / hourMillisec);
+    let totalMins = Math.floor(difference / minMillisec);
+    let totalSecs = Math.floor(difference / secMillisec);
+
+    const totalTimeValues = [totalMonths, totalWeeks, totalDays, totalHours, totalMins, totalSecs];
+
+    totalTimeText.map((unit, index) => {
+        totalTimeValues[index] = timeFormatter.format(totalTimeValues[index]);
+        unit.textContent = `Totaal: ${totalTimeValues[index]}`;
+    })
 }, 1000);
 
 const addZeroBefore = (timeUnit) => {
